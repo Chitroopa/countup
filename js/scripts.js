73a4b18932df4c545e66486fe9ab3cbe84da1521
Chitroopa Manikkavasagam and LaTanya Donaldson <latanya@>
2017-03-28 09:53:47 -0700

    add input validations

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,25 @@ $(document).ready(function() {
     var totalNumber = [];
     var num1Input = parseInt($("#num1").val());
     var num2Input = parseInt($("#num2").val());
-    for (var index = num2Input; index <= num1Input; index += num2Input)
+    if( num1Input && num2Input)
     {
-      totalNumber.push(index);
-    }
-    $("p").text(totalNumber);
+      if((num1Input > 0 && num2Input > 0) && (num1Input > num2Input))
+      {
+        for (var index = num2Input; index <= num1Input; index += num2Input)
+        {
+          totalNumber.push(index);
+        }
+        $("p").text(totalNumber);
+      }
+      else
+      {
+        alert("Error : negative number or count by number is larger than the count to numbers");
 
+      }
+    }
+    else
+    {
+      alert("Please enter all information")
+    }
   });
 });
